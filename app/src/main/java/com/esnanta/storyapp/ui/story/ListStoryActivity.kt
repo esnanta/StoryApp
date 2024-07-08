f98ce2df80e440ada442d1f56fffb7c5d85a93ab
Nanta Es <ombakrinai@gmail.com>
2024-07-08 21:43:39 +0700

    add toolbar to list and detail story

--- app/src/main/java/com/esnanta/storyapp/ui/story/ListStoryActivity.kt
+++ app/src/main/java/com/esnanta/storyapp/ui/story/ListStoryActivity.kt
@@ -1,11 +1,14 @@
 package com.esnanta.storyapp.ui.story
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.esnanta.storyapp.R
 import com.esnanta.storyapp.data.source.remote.Result
 import com.esnanta.storyapp.databinding.ActivityListStoryBinding
 import com.esnanta.storyapp.di.StoryViewModelFactory
@@ -23,9 +26,32 @@ class ListStoryActivity : AppCompatActivity() {
         binding = ActivityListStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         setupRecyclerView()
         observeViewModel()
         viewModel.fetchListStory()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_home -> {
+                // Handle home action
+                true
+            }
+            R.id.action_log_out -> {
+                // Handle log out action
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun setupRecyclerView() {
--- app/src/main/java/com/esnanta/storyapp/ui/storydetail/DetailStoryActivity.kt
+++ app/src/main/java/com/esnanta/storyapp/ui/storydetail/DetailStoryActivity.kt
@@ -1,11 +1,14 @@
 package com.esnanta.storyapp.ui.storydetail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.esnanta.storyapp.R
 import com.esnanta.storyapp.data.source.remote.Result
 import com.esnanta.storyapp.databinding.ActivityDetailStoryBinding
 import com.esnanta.storyapp.di.StoryViewModelFactory
@@ -22,12 +25,38 @@ class DetailStoryActivity : AppCompatActivity() {
         binding = ActivityDetailStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val storyId = intent.getStringExtra(EXTRA_STORY_ID)
         if (storyId != null) {
             viewModel.fetchStoryDetail(storyId)
         }
 
         observeViewModel()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_home -> {
+                // Handle home action
+                true
+            }
+            R.id.action_log_out -> {
+                // Handle log out action
+                true
+            }
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun observeViewModel() {
