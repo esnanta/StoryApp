37fc26044cf62e4b97ff9e3c81d32497ed405eac
Nanta Es <ombakrinai@gmail.com>
2024-08-04 15:53:50 +0700

    add ListStoryViewModelTest.kt. still error. remove ExampleUnitTest.kt

---
+++ app/src/test/java/com/esnanta/storyapp/ui/story/ListStoryViewModelTest.kt
@@ -0,0 +1,106 @@
+package com.esnanta.storyapp.ui.story
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import androidx.paging.AsyncPagingDataDiffer
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import androidx.recyclerview.widget.ListUpdateCallback
+import com.esnanta.storyapp.DataDummy
+import com.esnanta.storyapp.MainDispatcherRule
+import com.esnanta.storyapp.data.repository.StoryRepository
+import com.esnanta.storyapp.data.source.remote.response.ListStoryItem
+import com.esnanta.storyapp.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class ListStoryViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainDispatcherRules = MainDispatcherRule()
+
+    @Mock
+    private lateinit var storyRepository: StoryRepository
+
+    @Test
+    fun `when Get Stories Should Not Null and Return Data`() = runTest {
+        val dummyStories = DataDummy.generateDummyListStoryItem()
+        val data: PagingData<ListStoryItem> = StoryPagingSource.snapshot(dummyStories)
+        val expectedStories = MutableLiveData<PagingData<ListStoryItem>>()
+        expectedStories.value = data
+        Mockito.`when`(storyRepository.getListStory()).thenReturn(expectedStories.asFlow())
+
+        val listStoryViewModel = ListStoryViewModel(storyRepository)
+        val actualStories: PagingData<ListStoryItem> = listStoryViewModel.listStory.getOrAwaitValue()
+
+        val differ = AsyncPagingDataDiffer(
+            diffCallback = StoryDiffCallback(),
+            updateCallback = noopListUpdateCallback,
+            workerDispatcher = Dispatchers.Main,
+        )
+        differ.submitData(actualStories)
+
+        assertNotNull(differ.snapshot())
+        assertEquals(dummyStories.size, differ.snapshot().size)
+        assertEquals(dummyStories[0], differ.snapshot()[0])
+    }
+
+    @Test
+    fun `when Get Stories Empty Should Return No Data`() = runTest {
+        val data: PagingData<ListStoryItem> = PagingData.from(emptyList())
+        val expectedStories = MutableLiveData<PagingData<ListStoryItem>>()
+        expectedStories.value = data
+        Mockito.`when`(storyRepository.getListStory()).thenReturn(expectedStories.asFlow())
+
+        val listStoryViewModel = ListStoryViewModel(storyRepository)
+        val actualStories: PagingData<ListStoryItem> = listStoryViewModel.listStory.getOrAwaitValue()
+
+        val differ = AsyncPagingDataDiffer(
+            diffCallback = StoryDiffCallback(),
+            updateCallback = noopListUpdateCallback,
+            workerDispatcher = Dispatchers.Main,
+        )
+        differ.submitData(actualStories)
+
+        assertEquals(0, differ.snapshot().size)
+    }
+}
+
+class StoryPagingSource : PagingSource<Int, LiveData<List<ListStoryItem>>>() {
+    companion object {
+        fun snapshot(items: List<ListStoryItem>): PagingData<ListStoryItem> {
+            return PagingData.from(items)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, LiveData<List<ListStoryItem>>>): Int {
+        return 0
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LiveData<List<ListStoryItem>>> {
+        return LoadResult.Page(emptyList(), 0, 1)
+    }
+}
+
+val noopListUpdateCallback = object : ListUpdateCallback {
+    override fun onInserted(position: Int, count: Int) {}
+    override fun onRemoved(position: Int, count: Int) {}
+    override fun onMoved(fromPosition: Int, toPosition: Int) {}
+    override fun onChanged(position: Int, count: Int, payload: Any?) {}
+}
